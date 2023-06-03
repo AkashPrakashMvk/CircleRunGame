849a0794cc6a793913c1d0c3e907c6efb83e3d59
akashprakashmvk <akashponnaramthothan@gmail.com>
2023-06-03 16:44:32 +0530

    OpenLinkAdded

---
+++ Assets/Scripts/Urls.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Urls : MonoBehaviour
+{
+public void OnMouseDown()
+{
+    Application.OpenURL("https://mez.ink/akashprakash");
+}
+}
